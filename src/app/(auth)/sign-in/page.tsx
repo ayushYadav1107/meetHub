fb421220524e535da9ea540dfa3524c6bf7142fd
ayushYadav1107 <himmatyadav1995@gmail.com>
2025-08-24 22:51:56 +0530

    authentication-ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+function Page() {
+  
+  return <SignInView />;
+}
+
+export default Page;
+
+// http://localhost:3000/sign-in
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+function Page() {
+  
+  return(
+    <SignUpView/>
+  );
+}
+
+export default Page;
+
+// http://localhost:3000/sign-in
