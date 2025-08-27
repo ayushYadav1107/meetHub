21dd94a534b1f1d7e7998487518b408f747ef5b5
ayushYadav1107 <himmatyadav1995@gmail.com>
2025-08-27 17:36:25 +0530

    authentication_socials configured

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,18 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
+
 import React from "react";
-import { Card } from "@/components/ui/card";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 
-function Page() {
-  
+async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
   return <SignInView />;
 }
 
